43b0c31681b54a0e7dfef58304b5fcbe6c454bd3
Andrew Kerr <me+github@andrewjkerr.com>
2016-02-19 19:03:21 -0500

    added total counts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,17 @@ class StaticPagesController < ApplicationController
     arr.map { |x| x.delete("_id") }
     @sign_ins = arr.uniq
   end
+
+  def count
+    response = HTTParty.get('https://eweek2016.herokuapp.com/api/attendees')
+    arr = JSON.parse(response.body)
+    arr.map { |x| x.delete("_id") }
+    sign_ins = arr.uniq
+    @org_count = {}
+    sign_ins.each do |sign_in|
+      @org_count[sign_in['organization']] ||= 0
+      @org_count[sign_in['organization']] += 1
+    end
+    @org_count
+  end
 end
